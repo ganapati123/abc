8e050fe062e2fddd6b82dd85e6749f6567ae24f1
unknown <cganapati3@gmail.com>
2023-04-23 11:51:11 +0530

    first push

---
+++ login/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import Register from './Register';
+import Login from './Login';
+
+function App() {
+  return (
+      <main className='App'>
+         {/* <Register/> */}
+         <Login/>
+      </main>
+  );
+}
+
+export default App;
